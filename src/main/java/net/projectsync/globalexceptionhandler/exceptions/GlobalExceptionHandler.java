af71d129984654a8f1bf7a42c4e6f5abadbebd0b
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-08-21 17:35:30 +0530

    Initial commit

--- src/main/java/net/projectsync/globalexceptionhandler/exceptions/GlobalExceptionHandler.java
+++ src/main/java/net/projectsync/globalexceptionhandler/exceptions/GlobalExceptionHandler.java
@@ -3,15 +3,17 @@ package net.projectsync.globalexceptionhandler.exceptions;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -68,6 +70,41 @@ public class GlobalExceptionHandler {
                 .build();
         
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+    
+    /**
+     * Handler for incorrect/non-existent endpoints. Ex: /api/v1/<incorrectURI>/1
+     * Add below two properties in application.properties file
+     *	spring.mvc.throw-exception-if-no-handler-found=true
+     *	spring.web.resources.add-mappings=false
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NoHandlerFoundException ex, HttpServletRequest request) {
+        
+    	logger.info("incorrect endpoint called");
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message("No handler found for " + ex.getHttpMethod() + " " + ex.getRequestURL())
+                .details(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+    
+    // handle wrong HTTP method cases (e.g., sending POST /users/1 when only GET exists).
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
+        
+    	logger.info("incorrect method called");
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .message(String.format("Method %s not supported for this endpoint. Supported methods: ", ex.getMethod(), String.join(", ", ex.getSupportedMethods())))
+                .details(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .build();
+        
+        return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
     
     /**
